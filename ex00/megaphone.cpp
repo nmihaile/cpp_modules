1f323a82f85b399950f4c339938206b99c58cb78
nmihaile <nmihaile@student.42heilbronn.de>
2024-06-29 12:22:45 +0200

    changes megaphone.cpp to be implemented more in a c++ manner

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,23 +6,23 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 11:33:28 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/06/29 11:33:33 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/06/29 12:18:24 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-void	shout(char *str)
+void	shout(std::string str)
 {
-	for (int i = 0; str[i]; i++)
-		std::cout << (char)std::toupper(str[i]);
+	for (std::string::iterator it = str.begin(); it < str.end(); ++it)
+		std::cout << (char)std::toupper(*it);
 }
 
 int	main(int ac, char **av)
 {
 	if (ac > 1)
 		for (int i = 1; i < ac; i++)
-			shout(av[i]);
+			shout(std::string(av[i]));
 	else
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	std::cout << std::endl;
