cfde4e170f415ac791f6f1761493ba8c0fa5e5b8
nmihaile <nmihaile@student.42heilbronn.de>
2024-07-03 18:13:13 +0200

    updates main.cpp

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,19 +6,35 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 11:34:20 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/07/01 11:24:06 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/07/03 18:00:44 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/phonebook_header.h"
 
+void	init_input(t_input *input)
+{
+	input->status = PB_NONE;
+	input->str = "";
+	input->error = "";
+	input->printed_lines = 0;	
+}
+
 int	main(void)
 {
-	Contact c("foo", "bar", "nick", "01", "sec");
+	PhoneBook 	phonebook;
+	t_input		input;
 	
-	// c.first_name = "John Dow";
-	
-	std::cout << c.first_name << std::endl;
-
+	init_input(&input);
+	while (42)
+	{
+		input.status = PB_NONE;
+		if (getInput(&input, "ADD, SEARCH or EXIT", NULL, &render_phonebook, &check_main_input) == PB_ERROR)
+			continue ;
+		if (input.status == PB_EXIT)
+			break ;
+		execute_cmd(&input, &phonebook);
+	}
+	clear_lines(input.printed_lines);
 	return (0);
 }
