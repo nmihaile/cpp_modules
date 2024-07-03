a2326d970930c0ad8c8f36356c6b67c54b65b3a1
nmihaile <nmihaile@student.42heilbronn.de>
2024-07-03 18:15:42 +0200

    adds validations.cpp

---
+++ ex01/src/validations.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validations.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 18:06:05 by nmihaile          #+#    #+#             */
+/*   Updated: 2024/07/03 11:27:23 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/phonebook_header.h"
+
+bool	is_valid_string(std::string *str, t_input *input)
+{
+	if (str->length() == 0)
+	{
+		input->error = "No input. At least 1 character expected.";
+		*str = "";
+		return (false);
+	}
+	for (std::string::iterator it = str->begin(); it < str->end(); ++it)
+		if (!(std::isalpha(*it) || *it == ' '))
+		{
+			input->error = "Invalid characters. Use [A..Z, a..z, space]";
+			return (false);
+		}
+	return (true);
+}
+
+bool	is_valid_number(std::string *str, t_input *input)
+{
+	int	offset;
+	
+	offset = 0;
+	if (str->length() == 0)
+	{
+		input->error = "No input. At least 1 character expected.";
+		*str = "";
+		return (false);
+	}
+	if (str->length() > 1)
+		offset = 1;
+	if (!(str->at(0) == '+' || str->at(0) == '0'))
+	{
+		input->error = "Start phone number with + or 0";
+		*str = "";
+		return (false);
+	}
+	for (std::string::iterator it = str->begin() + offset; it < str->end(); ++it)
+		if (!(std::isdigit(*it) || *it == ' '))
+		{
+			input->error = "Invalid characters. Use [1..9, space]";
+			return (false);
+		}
+	return (true);
+}
