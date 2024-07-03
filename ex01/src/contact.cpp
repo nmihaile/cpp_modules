ad47615b98c5a448d1b665539e47649d46babe3a
nmihaile <nmihaile@student.42heilbronn.de>
2024-07-03 18:11:55 +0200

    updates Contact.cpp

--- ex01/src/contact.cpp
+++ ex01/src/contact.cpp
@@ -6,18 +6,68 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 11:38:26 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/07/01 11:00:14 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/07/01 16:22:40 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/contact.hpp"
 
-// Contact::Contact()
-// {
+Contact::Contact()
+{
 	
-// }
+}
 
 Contact::~Contact()
 {
 	
+}
+
+void	Contact::set_first_name(std::string fn)
+{
+	first_name = fn;
+}
+
+std::string	Contact::get_first_name(void)
+{
+	return (first_name);
+}
+
+void	Contact::set_last_name(std::string ln)
+{
+	last_name = ln;
+}
+
+std::string	Contact::get_last_name(void)
+{
+	return (last_name);
+}
+
+void	Contact::set_nickname(std::string nn)
+{
+	nickname = nn;
+}
+
+std::string	Contact::get_nickname(void)
+{
+	return (nickname);
+}
+
+void	Contact::set_phone_number(std::string pn)
+{
+	phone_number = pn;
+}
+
+std::string	Contact::get_phone_number(void)
+{
+	return (phone_number);
+}
+
+void	Contact::set_darkest_secret(std::string ds)
+{
+	darkest_secret = ds;
+}
+
+std::string	Contact::get_darkest_secret(void)
+{
+	return (darkest_secret);
 }
