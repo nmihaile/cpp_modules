771aceb695e2e8d5701624530c3995e9550880b0
nmihaile <nmihaile@student.42heilbronn.de>
2024-07-07 19:06:24 +0200

    adds constructor & destructor debug info && adds color to ex01 header prints

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 16:33:41 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/07/07 18:35:05 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/07/07 18:54:50 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 Zombie::Zombie(void) : m_name("Unnamed Zombie")
 {
+	std::cout << "created: " << m_name << std::endl;
 }
 
 Zombie::Zombie(std::string name) : m_name(name)
@@ -22,6 +23,7 @@ Zombie::Zombie(std::string name) : m_name(name)
 
 Zombie::~Zombie(void)
 {	
+	std::cout << m_name << ": deleted" << std::endl;
 }
 
 void	Zombie::set_name(std::string new_name)
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,30 +6,46 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 16:39:01 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/07/07 18:47:23 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/07/07 19:04:12 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
 #include "Zombie.hpp"
 
-#define ZombieCount 25
+# define LIGHTRED		"\033[0;91m"
+# define LIGHTGREEN		"\033[0;92m"
+# define LIGHTYELLOW	"\033[0;93m"
+# define LIGHTBLUE		"\033[0;94m"
+# define LIGHTMAGENTA	"\033[0;95m"
+# define LIGHTCYAN		"\033[0;96m"
+# define RESET			"\033[0m"
+
+#define ZombieCount 5
+
+void	print_section(std::string title, std::string color)
+{
+	std::cout	<< color
+				<< "\n--------------------------------\n"
+				<< title
+				<< "\n--------------------------------" << RESET << std::endl;	
+}
 
 int	main(void)
 {
 	Zombie *zHord;
 
-	std::cout	<< "--------------------------------\n"
-				<< "Creates Zombie Hord (" << ZombieCount << "):\n"
-				<< "--------------------------------" << std::endl;
-	
+	print_section("Create zombie hord", LIGHTGREEN);
 	zHord = zombieHorde(ZombieCount, "Hungry Hannah");
 
 	if (zHord == NULL)
 		std::cout << "Error: memory for zombieHord could not be allocated." << std::endl;
 	else
 	{
+		print_section("Announce zombie hord", LIGHTCYAN);
 		for (int i = 0; i < ZombieCount; i++)
 			zHord[i].announce();
+		print_section("Delete zombie hord", LIGHTRED);
 		delete[](zHord);
 	}
 
