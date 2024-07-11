41ffe105cda310359f9348be23ad13cbbdac0b7b
nmihaile <nmihaile@student.42heilbronn.de>
2024-07-11 11:21:52 +0200

    adds ex04 with tests

---
+++ ex04/ft_replace.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_replace.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 18:47:34 by nmihaile          #+#    #+#             */
+/*   Updated: 2024/07/09 18:51:32 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_REPLACE
+#define FT_REPLACE
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+bool	ft_replace(std::string inFileName, std::string s1, std::string s2);
+
+#endif
