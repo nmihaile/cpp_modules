83160756fdc4b6fca2ef767038ed0833fb3c68dd
nmihaile <nmihaile@student.42heilbronn.de>
2024-07-17 12:13:19 +0200

    adds inital ex03 /> inital commit with main and Fixed class

---
+++ ex03/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 18:12:42 by nmihaile          #+#    #+#             */
+/*   Updated: 2024/07/17 12:11:53 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <climits>
+#include "Fixed.hpp"
+
+#define LIGHTRED		"\033[0;91m"
+#define LIGHTGREEN		"\033[0;92m"
+#define LIGHTYELLOW		"\033[0;93m"
+#define LIGHTBLUE		"\033[0;94m"
+#define LIGHTMAGENTA	"\033[0;95m"
+#define LIGHTCYAN		"\033[0;96m"
+#define DARKGRAY		"\033[0330;90m"
+#define RESET			"\033[0m"
+
+
+int	main(void)
+{
+
+	std::cout << "Welcome to ex03" << std::endl;
+	
+	return (0);
+}
