3b60c4314985fd1fdcf0c510a49ab1e110d85c0e
nmihaile <nmihaile@student.42heilbronn.de>
2024-07-26 21:33:44 +0200

    ex01 adds Ideas to Brain class && adds a add_idea as setter for ideas

--- ex01/Brain.cpp
+++ ex01/Brain.cpp
@@ -6,13 +6,13 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 17:14:38 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/07/26 18:26:10 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/07/26 21:18:27 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
-Brain::Brain()
+Brain::Brain() : m_next_idea(0)
 {
 }
 
@@ -39,8 +39,22 @@ Brain& Brain::operator = (const Brain& other)
 /* ************************************************************************** */
 
 
+void	Brain::add_idea(std::string idea)
+{
+	ideas[m_next_idea++] = idea;
+	if (m_next_idea >= MAX_IDEAS)
+		m_next_idea = 0;
+}
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
 void	Brain::deepcpy_ideas(const Brain& other)
 {
+	m_next_idea = other.m_next_idea;
+	
 	for (int it = 0; it < MAX_IDEAS; it++)
 		ideas[it] = other.ideas[it];	
 }
--- ex01/Brain.hpp
+++ ex01/Brain.hpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 17:14:40 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/07/26 18:25:55 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/07/26 19:46:05 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,11 @@ public:
 
 	Brain& operator = (const Brain& other);
 
+	void	add_idea(std::string idea);
+
 private:
+	int		m_next_idea;
+
 	void	deepcpy_ideas(const Brain& other);
 };
 
