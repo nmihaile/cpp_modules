7804730a38772c6941c7a4ad139e9c5bb17757f3
nmihaile <nmihaile@student.42heilbronn.de>
2024-12-13 11:51:20 +0100

    ex01 adds authority.h

---
+++ ex01/authority.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   authority.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 15:47:41 by nmihaile          #+#    #+#             */
+/*   Updated: 2024/12/12 20:21:30 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AUTHORITY_H
+#define AUTHORITY_H
+
+#define GREEN			"\033[32m"
+#define BLUE			"\033[34m"
+#define LIGHTRED		"\033[91m"
+#define LIGHTGREEN		"\033[92m"
+#define LIGHTYELLOW		"\033[93m"
+#define LIGHTBLUE		"\033[94m"
+#define LIGHTMAGENTA	"\033[95m"
+#define LIGHTCYAN		"\033[96m"
+#define RESET			"\033[0m"
+
+#define HIGHEST_GRADE	1
+#define LOWEST_GRADE	150
+
+#endif
