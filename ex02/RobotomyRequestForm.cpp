d0271015aa721769073ef6d393a54670ce5b5233
nmihaile <nmihaile@student.42heilbronn.de>
2024-12-15 16:41:00 +0100

    ex02: implements the noisy drilling RobotomyRequestForm::execute() function

--- ex02/RobotomyRequestForm.cpp
+++ ex02/RobotomyRequestForm.cpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 19:56:09 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/12/14 20:38:18 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/12/15 15:58:02 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 RobotomyRequestForm::RobotomyRequestForm(std::string _target)
 	: AForm(_target, 72, 75)
 {
+	std::srand(std::time(nullptr));
 }
 
 RobotomyRequestForm::~RobotomyRequestForm()
@@ -26,7 +27,45 @@ RobotomyRequestForm::~RobotomyRequestForm()
 /* ************************************************************************** */
 
 
-const std::string	drill_sounds[20] = {
+void	RobotomyRequestForm::execute(const Bureaucrat& executor) const
+{
+	validateGrade(getGradeToExec());
+	if (executor.getGrade() < HIGHEST_GRADE || executor.getGrade() > LOWEST_GRADE)
+		throw (std::string("Executor has INAVLID_GRADE!"));
+
+	checkForm(executor);
+
+	// make drilling noises
+	std::cout << "🔊 ";
+	int noise_count = std::rand() % 20 + 10;
+	for (int i = noise_count; i--;)
+	{
+		if (i < noise_count - 1)
+			std::cout << drill_colors[std::rand() % 6] << "-";
+		else
+			std::cout << drill_colors[std::rand() % 6] << "";
+
+		std::cout << drill_colors[std::rand() % 6] << drill_sounds[std::rand() % 20] << std::flush;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	}
+
+	if (std::rand() % 2 == 0)
+		std::cout	<< RESET << std::endl << "\""
+					<< getName() << "\""
+					<< " has been robotomized." << std::endl;
+	else
+		std::cout	<< RESET << std::endl
+					<< "Failed to robotomy target: " << "\""
+					<< getName() << "\"" << std::endl;
+}
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+const std::string	RobotomyRequestForm::drill_sounds[20] = {
 	"Brr",
 	"Vrr",
 	"Zzz",
@@ -47,4 +86,13 @@ const std::string	drill_sounds[20] = {
 	"Rrt",
 	"Grk",
 	"Dzt"
+};
+
+const std::string	RobotomyRequestForm::drill_colors[6] = {
+	"\033[91m",
+	"\033[92m",
+	"\033[93m",
+	"\033[94m",
+	"\033[95m",
+	"\033[96m"
 };
--- ex02/RobotomyRequestForm.hpp
+++ ex02/RobotomyRequestForm.hpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 19:56:11 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/12/14 20:36:29 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/12/15 15:39:05 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,30 @@
 #define ROBOTOMY_REQUEST_FORM_HPP
 
 #include "AForm.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <thread>
+
+#define LIGHTRED		"\033[91m"
+#define LIGHTGREEN		"\033[92m"
+#define LIGHTYELLOW		"\033[93m"
+#define LIGHTBLUE		"\033[94m"
+#define LIGHTMAGENTA	"\033[95m"
+#define LIGHTCYAN		"\033[96m"
+#define RESET			"\033[0m"
 
 class RobotomyRequestForm : public AForm
 {
 public:
 	RobotomyRequestForm(std::string _target);
 	~RobotomyRequestForm() override;
-	
+
+	void	execute(const Bureaucrat& executor) const override;
+
 private:
 	static const std::string	drill_sounds[20];
+	static const std::string	drill_colors[6];
 };
 
 #endif
