caadb150122f74c36b91fce3d9e445510745d15f
nmihaile <nmihaile@student.42heilbronn.de>
2024-12-17 19:49:42 +0100

    ex03: adds rainbow_print to main.cpp

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 15:08:21 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/12/17 17:43:43 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/12/17 19:32:02 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 #include "Bureaucrat.hpp"
 #include "Intern.hpp"
 
+#define BOLD			"\033[1m"
 #define GREEN			"\033[32m"
 #define BLUE			"\033[34m"
 #define LIGHTRED		"\033[91m"
@@ -29,11 +30,31 @@
 #define LIGHTCYAN		"\033[96m"
 #define RESET			"\033[0m"
 
+static const char *rainbow_colors[6] = {
+	"\033[91m",
+	"\033[93m",
+	"\033[92m",
+	"\033[96m",
+	"\033[94m",
+	"\033[95m"
+};
+
+void	rainbow_print(std::string str)
+{
+	static size_t	col;
+	std::cout << BOLD;
+	for(std::string::iterator it = str.begin(); it < str.end(); ++it)
+	{
+		std::cout << rainbow_colors[col++ % 6] << *it;
+	}
+	std::cout << RESET <<std::endl;
+}
 
 int	main()
 {
 
-	std::cout << "Interns Welcome" << std::endl;
+	// std::cout << "Interns are Welcome" << std::endl;
+	rainbow_print(".~ Interns are Welcome ~.");
 
 	Intern intern;
 
@@ -47,7 +68,7 @@ int	main()
 
 	try
 	{
-		form = intern.makeForm("enrole 42", "R2-D2");
+		form = intern.makeForm("enrole @ 42", "student");
 		std::cout << *form << std::endl;
 	}
 	catch(const std::exception& e)
