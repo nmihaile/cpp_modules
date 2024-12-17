236a807de84c91d3f44c4b6eb6415c4472d7fc22
nmihaile <nmihaile@student.42heilbronn.de>
2024-12-17 15:37:21 +0100

    ex03: adds Makefile and intal main.cpp

---
+++ ex03/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 15:08:21 by nmihaile          #+#    #+#             */
+/*   Updated: 2024/12/17 15:09:37 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "Bureaucrat.hpp"
+
+#define GREEN			"\033[32m"
+#define BLUE			"\033[34m"
+#define LIGHTRED		"\033[91m"
+#define LIGHTGREEN		"\033[92m"
+#define LIGHTYELLOW		"\033[93m"
+#define LIGHTBLUE		"\033[94m"
+#define LIGHTMAGENTA	"\033[95m"
+#define LIGHTCYAN		"\033[96m"
+#define RESET			"\033[0m"
+
+
+int	main()
+{
+
+	std::cout << "Interns Welcome" << std::endl;
+
+	return (0);
+}
