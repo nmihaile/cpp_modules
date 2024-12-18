47cb72915ceebcc1afa81fd57ce8e10929657331
nmihaile <nmihaile@student.42heilbronn.de>
2024-12-18 20:07:37 +0100

    ex03: updates Inter to use conventional func pointer && uses std::string::comapare()

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,27 +6,43 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 15:39:25 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/12/18 17:58:32 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/12/18 19:59:56 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
 
-const char	*Intern::formSpecifiers[Intern::FORM_COUNT] = {
+const std::string	Intern::formSpecifiers[Intern::FORM_COUNT] = {
 	"shrubbery creation",
 	"robotomy request",
 	"presidential pardon"
 };
 
-const std::function<AForm*(std::string& _target)>	Intern::formFactory[Intern::FORM_COUNT] = {
-	[](std::string& _target) { return ( new ShrubberyCreationForm(_target) ); },
-	[](std::string& _target) { return ( new RobotomyRequestForm(_target) ); },
-	[](std::string& _target) { return ( new PresidentialPardonForm(_target) ); }
+const Intern::createFunc	Intern::formFactory[Intern::FORM_COUNT] = {
+	&Intern::createSchrubberyCreationForm,
+	&Intern::createRobotomyRequestForm,
+	&Intern::createPresidentialPardonForm
 };
 
-// Lambdas functions are inline, anonymous functions
-// [] is the capture list. An empty list ([]) means the lambda does not capture
-// any variables from its surrounding scope
+AForm	*Intern::createSchrubberyCreationForm(const std::string& _target)
+{
+	return ( new ShrubberyCreationForm(_target) );
+}
+
+AForm	*Intern::createRobotomyRequestForm(const std::string& _target)
+{
+	return ( new RobotomyRequestForm(_target) );
+}
+
+AForm	*Intern::createPresidentialPardonForm(const std::string& _target)
+{
+	return ( new PresidentialPardonForm(_target) );
+}
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
 
 Intern::Intern()
 {
@@ -53,7 +69,7 @@ Intern::~Intern()
 AForm	*Intern::makeForm(std::string _name, std::string _target)
 {
 	for (size_t i = 0; i < Intern::FORM_COUNT; ++i)
-		if (std::strcmp(_name.c_str(), formSpecifiers[i]) == 0)
+		if (formSpecifiers[i].compare(_name) == 0)
 			return( formFactory[i](_target) );
 	throw ( Intern::InvalidFormSpecifierException() );
 	return (nullptr);
--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 15:39:27 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/12/18 16:58:25 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/12/18 20:00:52 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 
 #include <string>
 #include <exception>
-#include <functional>
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
@@ -36,10 +35,17 @@ public:
 	};
 
 private:
-	static const char											*formSpecifiers[Intern::FORM_COUNT];
-	static const std::function<AForm*(std::string& _target)>	formFactory[Intern::FORM_COUNT];
+	typedef AForm				*(*createFunc)(const std::string&);
+
+	static const std::string	formSpecifiers[Intern::FORM_COUNT];
+	static const createFunc		formFactory[Intern::FORM_COUNT];
+	static AForm				*createSchrubberyCreationForm(const std::string& _target);
+	static AForm				*createRobotomyRequestForm(const std::string& _target);
+	static AForm				*createPresidentialPardonForm(const std::string& _target);
+
 	Intern(const Intern&) = delete;
 	Intern&	operator=(const Intern&) = delete;
+
 };
 
 #endif
