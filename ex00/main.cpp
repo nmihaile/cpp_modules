0ee43f3a4498d9c633197e52eeedbecc79c7c126
nmihaile <nmihaile@student.42heilbronn.de>
2024-12-20 21:30:39 +0100

    ex00: updates Makefile and main.cpp to work with the experimental ScalarConverter implementation

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,26 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 18:25:37 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/12/19 19:10:58 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/12/20 15:01:35 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main()
+#include "ScalarConverter.hpp"
+
+int	main(int ac, char **av)
 {
+	if (ac == 2)
+		ScalarConverter::convert(av[1]);
+
+	// Literall Types
+	// singel character
+
+	// num chars -> aka int
+	// 123.123f / -inff / inff
+	// -> float
+	// 234.234 -> double
+	//  -inf  inf
+	//
 
 	return (0);
 }
