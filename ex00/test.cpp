f4b30747bf3c796490a0f43a74e05e65ac1d21bc
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-04 13:25:18 +0100

    ex00: adds test.cpp and test comments in main.cpp

---
+++ ex00/test.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+int	main(void)
+{
+
+	std::cout << "Welcom to test ScalarConvert" << std::endl;
+	std::cout << "----------------------------" << std::endl;
+
+	return (0);
+}
