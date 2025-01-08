e7669fe5425a8c62be948f9c271cf26be1a18e3d
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-08 19:44:47 +0100

    ex02: adds class A, B and C in ABC.hpp

---
+++ ex02/ABC.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ABC.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 16:26:12 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/08 16:42:37 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef ABC_HPP
+#define ABC_HPP
+
+#include "Base.hpp"
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+#endif
