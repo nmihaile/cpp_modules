e72c8485898b27604875b1652eeba1c35b091661
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-08 19:45:59 +0100

    ex02: adds A,B,C  generator function and type identification functions in utils.hpp/.cpp

---
+++ ex02/utils.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 17:00:58 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/08 18:11:10 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.hpp"
+
+Base*	generate(void)
+{
+	static std::random_device					rd;
+	static std::mt19937							gen(rd());
+	static std::uniform_int_distribution<int>	dis(0, 2);
+
+	Base*	obj;
+
+	switch (dis(gen))
+	{
+		case (0):
+			obj = new A;
+			break ;
+		case (1):
+			obj = new B;
+			break ;
+		case (2):
+			obj = new C;
+			break ;
+		default:
+			obj = nullptr;
+	}
+	return ( obj );
+}
+
+void	identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void	identify(Base& p)
+{
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+		return ;
+	}
+	catch(...) { }
+
+	try
+	{
+		(void)dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+		return ;
+	}
+	catch(...) { }
+
+	try
+	{
+		(void)dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+		return ;
+	}
+	catch(...) { }
+}
+
