708121f7ff8a91be9a050369afee3d244bc0b1ce
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-11 20:30:21 +0100

    ex00: adds template functions: swap, min, max in whatever.hpp

---
+++ ex00/whatever.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/11 13:24:14 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/11 19:54:01 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template<typename T>
+void	swap(T& a, T&b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template<typename T>
+T	min(T& a, T&b)
+{
+	return ( a <= b ? a : b );
+}
+
+template<typename T>
+T	max(T& a, T&b)
+{
+	return ( a >= b ? a : b );
+}
+
+#endif
