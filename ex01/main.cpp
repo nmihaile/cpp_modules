b83129de9c60450a10aa8f79af129891ff0c622e
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-13 17:09:40 +0100

    ex01: adds main && Makefile

---
+++ ex01/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/12 12:18:59 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/13 17:04:35 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "iter.hpp"
+#include "tester.hpp"
+#include "tests.hpp"
+
+int	main(void)
+{
+
+	test<int>(test_int_timesTwo     	, 11, std::string("0 2 4 6 8 10 12 14 16 18 20 ") );
+	test<int>(test_string_capitalize	,  3, std::string("HALLO WORLD WHERE ARE YOU ") );
+	test<int>(test_array_size_zero		,  0, "");
+	test<int>(test_array_size_one		,  1, "84 ");
+	test<int>(test_NULL_array			, 10, "Array pointer must not be NULL\n");
+	test<int>(test_array_same_values	, 10, "10 10 10 10 10 10 10 10 10 10 ");
+
+	print_test_result("");
+
+	return (0);
+}
