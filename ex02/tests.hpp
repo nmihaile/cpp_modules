53c75860bb04de8e2f8f96b7c60324dd07747ac3
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-15 17:01:36 +0100

    ex02: introduces tests .hpp .cpp WIP…

---
+++ ex02/tests.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 16:54:00 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/15 16:56:40 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef TESTS_HPP
+#define TESTS_HPP
+
+#include "Array.hpp"
+
+void	test_int_arr_0()
+{
+	Array<int>	arr(0);
+
+}
+
+#endif
