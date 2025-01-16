41016008720fcaec853bab43394c3546a71a8c75
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-16 09:40:57 +0100

    ex02: adds the first 7 test in main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 11:10:34 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/15 16:57:44 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/15 20:51:33 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,16 @@ int	main(void)
 	// for (unsigned int it = 0; it < arr_str.size(); ++it)
 	// 	std::cout << arr_str[it] << std::endl;
 
+	TEST(test_access_of_int_arr_0, Array<int>(0), "Index out of range\n");
+	TEST(test_valid_int_array, Array<int>(10), "0 1 2 3 4 5 6 7 8 9 ");
+	TEST(test_int_array_constructor_initialization, Array<int>(10), "0 0 0 0 0 0 0 0 0 0 ");
+	TEST(test_int_array_copy_assignemnt_op, Array<int>(10), "0 1 2 3 4 5 6 7 8 9 ");
+	
+	TEST(test_int_array_size_method, Array<int>(0), "0");
+	TEST(test_int_array_size_method, Array<int>(42), "42");
+	TEST(test_assignemnt_of_item, Array<int>(11), "42");
+
+	print_test_result("", "");
 
 	Array<int>			arr(10);
 	// std::cout << arr.m_items[0] << std::endl;
