85338fc9e93a3dd135ce17107510a8b59fd2da53
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-18 16:37:59 +0100

    ex01: adds addNumber && shortestSpan to Span class

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,13 +6,13 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 15:23:07 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/18 15:38:35 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/18 16:36:29 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-Span::Span(unsigned int n) : m_capacity(n), m_items(nullptr)
+Span::Span(unsigned int n) : m_capacity(n), m_size(0), m_items(nullptr)
 {
 	if (m_capacity > 0)
 		m_items = new int[m_capacity]();
@@ -36,12 +36,40 @@ Span&	Span::operator=(const Span& rhs)
 		if (m_items)
 			delete[](m_items);
 		m_capacity = rhs.m_capacity;
+		m_size = rhs.m_size;
+
 		if (m_capacity > 0)
 		{
 			m_items = new int[m_capacity];
-			for (unsigned int i = 0; i < m_capacity; ++i)
+			for (unsigned int i = 0; i < m_size; ++i)
 				m_items[i] = rhs.m_items[i];
 		}
 	}
 	return (*this);
 }
+
+void	Span::addNumber(int n)
+{
+	if (m_size >= m_capacity)
+		throw ( std::overflow_error("Span has reached its capacity, cannot add more elements.") );
+	m_items[m_size++] = n;
+}
+
+unsigned int	Span::shortesSpan(void)
+{
+	if (m_size <= 1)
+		throw ( std::logic_error("Not enough elements to calculate a span.") );
+
+	unsigned int	sp = std::numeric_limits<unsigned int>::max();
+	for (unsigned int i = 1; i < m_size; ++i)
+	{
+		unsigned int diff = (m_items[i] > m_items[i - 1])
+							? (m_items[i] - m_items[i - 1])
+							: (m_items[i - 1] - m_items[i]);
+		if (diff < sp)
+			sp = diff;
+	}
+	
+	return (sp);
+}
+
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 15:23:08 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/18 15:29:59 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/18 16:36:19 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,25 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <stdexcept>
+#include <limits>
+
 class Span
 {
 public:
 	Span(unsigned int n);
 	~Span();
 
-	Span&	operator=(const Span& rhs);
+	Span&			operator=(const Span& rhs);
+	void			addNumber(int n);
+	unsigned int	shortesSpan(void);
 	
 private:
 	Span() = delete;
 	Span(const Span& other) = delete;
 	
 	unsigned int	m_capacity;
+	unsigned int	m_size;
 	int*			m_items;
 };
 
