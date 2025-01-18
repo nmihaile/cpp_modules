3a525e285ce4bb6d9ab39f026f205dd0eba16f0f
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-18 13:00:08 +0100

    ex00: adds test for first and last el of vector and list

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,37 +6,59 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 21:41:12 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/18 12:17:43 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/18 12:59:36 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include <vector>
+#include <list>
 #include "easyfind.hpp"
 #include "tester.hpp"
 
-void	test_valid_vector_find(void)
+void	printDivider(void)
+{
+	std::cout << BLUE << "----------------------------------------------------------" << RESET << std::endl;
+}
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+void	test_find_existing_element_in_vector(void)
 {
 	std::vector<int>			arr = { 40, 41, 42, 43, 44 };
 	std::vector<int>::iterator	it = easyfind<std::vector<int>>(arr, 42);
 	std::cout << *it;
 }
 
-void	test_invalid_vector_find(void)
+void	test_find_first_element_in_vector(void)
+{
+	std::vector<int>			arr = { 40, 41, 42, 43, 44 };
+	std::vector<int>::iterator	it = easyfind<std::vector<int>>(arr, 40);
+	std::cout << *it;
+}
+
+void	test_find_last_element_in_vector(void)
+{
+	std::vector<int>			arr = { 40, 41, 42, 43, 44 };
+	std::vector<int>::iterator	it = easyfind<std::vector<int>>(arr, 44);
+	std::cout << *it;
+}
+
+void	test_find_nonexistent_element_in_vector(void)
 {
 	std::vector<int>			arr = { 30, 31, 32, 33, 34 };
 	std::vector<int>::iterator	it = easyfind<std::vector<int>>(arr, 42);
 	std::cout << *it;
 }
 
-void	test_empty_vector(void)
+void	test_find_in_empty_vector(void)
 {
 	std::vector<int>			arr;
 	std::vector<int>::iterator	it = easyfind<std::vector<int>>(arr, 42);
 	std::cout << *it;
 }
 
-void	test_huge_vector(void)
+void	test_find_in_huge_vector(void)
 {
 	std::vector<int>			arr;
 	arr.reserve(2000000);
@@ -47,37 +69,82 @@ void	test_huge_vector(void)
 	std::cout << *first << " " << *last;
 }
 
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+void	test_find_existing_element_in_list(void)
+{
+	std::list<int>				arr = { 40, 41, 42, 43, 44 };
+	std::list<int>::iterator	it = easyfind<std::list<int>>(arr, 42);
+	std::cout << *it;
+}
+
+void	test_find_first_element_in_list(void)
+{
+	std::list<int>				arr = { 40, 41, 42, 43, 44 };
+	std::list<int>::iterator	it = easyfind<std::list<int>>(arr, 40);
+	std::cout << *it;
+}
+
+void	test_find_last_element_in_list(void)
+{
+	std::list<int>				arr = { 40, 41, 42, 43, 44 };
+	std::list<int>::iterator	it = easyfind<std::list<int>>(arr, 44);
+	std::cout << *it;
+}
+
+void	test_find_nonexisting_element_in_list(void)
+{
+	std::list<int>				arr = { 30, 31, 32, 33, 34 };
+	std::list<int>::iterator	it = easyfind<std::list<int>>(arr, 42);
+	std::cout << *it;
+}
+
+void	test_find_in_empty_list(void)
+{
+	std::list<int>			arr;
+	std::list<int>::iterator	it = easyfind<std::list<int>>(arr, 42);
+	std::cout << *it;
+}
+
+void	test_find_in_huge_list(void)
+{
+	std::list<int>			arr;
+	for (size_t it = 0; it < 2000000; ++it)
+		arr.push_back(it);
+	std::list<int>::iterator	first = easyfind<std::list<int>>(arr, 0);
+	std::list<int>::iterator	last = easyfind<std::list<int>>(arr, 1999999);
+	std::cout << *first << " " << *last;
+}
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+
+
 int	main(void)
 {
 
-	TEST(test_valid_vector_find, "42");
-	TEST(test_invalid_vector_find, "element not found\n");
-	TEST(test_empty_vector, "element not found\n");
-	TEST(test_huge_vector, "0 19999999");
+	TEST(test_find_existing_element_in_vector, "42");
+	TEST(test_find_first_element_in_vector, "40");
+	TEST(test_find_last_element_in_vector, "44");
+	TEST(test_find_nonexistent_element_in_vector, "element not found\n");
+	TEST(test_find_in_empty_vector, "element not found\n");
+	TEST(test_find_in_huge_vector, "0 19999999");
 
-	// std::vector<int>	v, w;
+	printDivider();
 
-	// v.emplace_back(40);
-	// v.emplace_back(41);
-	// v.emplace_back(42);
-	// v.emplace_back(43);
-	// v.emplace_back(44);
-	// v.emplace_back(0);
-	// v.emplace_back(45);
+	TEST(test_find_existing_element_in_list, "42");
+	TEST(test_find_first_element_in_list, "40");
+	TEST(test_find_last_element_in_list, "44");
+	TEST(test_find_nonexisting_element_in_list, "element not found\n");
+	TEST(test_find_in_empty_list, "element not found\n");
+	TEST(test_find_in_huge_list, "0 1999999");
 
-	// try
-	// {
-	// 	// int si = 50;
-	// 	// std::vector<int>::iterator	it = easyfind<std::vector<int>>(v, 50);
-	// 	// std::cout << "found " << *it << std::endl;
-	// 	std::vector<int>::iterator	it = easyfind<std::vector<int>>(v, 42);
-	// 	std::cout << "found " << *it << std::endl;
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << '\n';
-	// }
-	
 
 	return (0);
 }
