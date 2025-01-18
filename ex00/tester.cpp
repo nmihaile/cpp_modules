7b74a48573cd86012adbe14139bdec5a00fe0e88
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-18 11:51:20 +0100

    ex00: adds tester: .hpp .tpp .cpp

---
+++ ex00/tester.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tester.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 11:35:08 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/18 11:39:59 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tester.hpp"
+
+size_t	test_count(bool do_count)
+{
+	static size_t	count;
+	if (do_count)
+		++count;
+	return (count);
+}
+
+size_t	test_passed(bool do_count)
+{
+	static size_t	count;
+	if (do_count)
+		++count;
+	return (count);
+}
+
+std::string	replace_underscores(std::string str)
+{
+	for (std::string::iterator it = str.begin(); it < str.end(); ++it)
+		if (*it == '_')
+			*it = ' ';
+	return (str);
+}
+
+void	print_test_result(std::string msg, std::string expected)
+{
+	if (test_passed(false) == test_count(false))
+	{
+		std::cout << std::endl;
+		std::cout << "  🎉🥳 All " << std::setw(2) << test_count(false) <<" tests passed! 🥳🎉" << std::endl;
+		std::cout << LIGHTGREEN << "══════════════════════════════════" << RESET << std::endl;
+	}
+	else
+	{
+		std::cout << LIGHTRED << "[KO]\n" << RESET << std::endl;
+		std::cout << LIGHTCYAN		<< "output  : [" <<  RESET << msg << LIGHTCYAN << "]" << RESET << std::endl;
+		if (!expected.empty())
+			std::cout << LIGHTCYAN	<< "expected: [" <<  RESET << expected << LIGHTCYAN << "]" << RESET << std::endl;
+
+		std::cout << "  ❌ " << LIGHTRED << "Failed at test: " << LIGHTCYAN << "[" << test_count(false) << "]" << std::endl;
+		std::cout << RESET << "     please investigate and debug accordingly…" << std::endl;
+		std::cout << LIGHTRED << "══════════════════════════════════════════════════" << RESET << std::endl;
+
+		exit(1);
+	}
+}
---
+++ ex00/tester.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tester.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 11:35:10 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/18 11:41:44 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef TESTER_HPP
+#define TESTER_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+#define BLUE			"\033[34m"
+#define LIGHTRED		"\033[91m"
+#define LIGHTGREEN		"\033[92m"
+#define LIGHTCYAN		"\033[96m"
+#define RESET			"\033[0m"
+
+#define TEST(func, input, expected) test(func, input, expected, #func)
+
+size_t		test_count(bool do_count);
+size_t		test_passed(bool do_count);
+std::string	replace_underscores(std::string str);
+void		print_test_result(std::string msg, std::string expected);
+
+#include "tester.tpp"
+
+#endif
