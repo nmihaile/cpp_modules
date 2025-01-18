8b229212685435e82e9d8de032cf4fe0855dfd1b
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-18 13:09:51 +0100

    ex00: adds test with array container

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,13 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 21:41:12 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/18 12:59:36 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/18 13:08:32 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
 #include <list>
+#include <array>
 #include "easyfind.hpp"
 #include "tester.hpp"
 
@@ -124,6 +125,50 @@ void	test_find_in_huge_list(void)
 /* ************************************************************************** */
 
 
+void	test_find_existing_element_in_array(void)
+{
+	std::array<int, 5>				arr = { 40, 41, 42, 43, 44 };
+	std::array<int, 5>::iterator	it = easyfind<std::array<int, 5>>(arr, 42);
+	std::cout << *it;
+}
+
+void	test_find_first_element_in_array(void)
+{
+	std::array<int, 5>				arr = { 40, 41, 42, 43, 44 };
+	std::array<int, 5>::iterator	it = easyfind<std::array<int, 5>>(arr, 40);
+	std::cout << *it;
+}
+
+void	test_find_last_element_in_array(void)
+{
+	std::array<int, 5>				arr = { 40, 41, 42, 43, 44 };
+	std::array<int, 5>::iterator	it = easyfind<std::array<int, 5>>(arr, 44);
+	std::cout << *it;
+}
+
+void	test_find_nonexisting_element_in_array(void)
+{
+	std::array<int, 5>				arr = { 30, 31, 32, 33, 34 };
+	std::array<int, 5>::iterator	it = easyfind<std::array<int, 5>>(arr, 42);
+	std::cout << *it;
+}
+
+void	test_find_in_empty_array(void)
+{
+	std::array<int, 0>			arr;
+	std::array<int, 0>::iterator	it = easyfind<std::array<int, 0>>(arr, 42);
+	std::cout << *it;
+}
+
+void	test_find_in_huge_array(void)
+{
+	std::array<int, 2000000>			arr;
+	for (size_t it = 0; it < 2000000; ++it)
+		arr[it] = it;
+	std::array<int, 2000000>::iterator	first = easyfind<std::array<int, 2000000>>(arr, 0);
+	std::array<int, 2000000>::iterator	last = easyfind<std::array<int, 2000000>>(arr, 1999999);
+	std::cout << *first << " " << *last;
+}
 
 
 int	main(void)
@@ -145,6 +190,14 @@ int	main(void)
 	TEST(test_find_in_empty_list, "element not found\n");
 	TEST(test_find_in_huge_list, "0 1999999");
 
+	printDivider();
+
+	TEST(test_find_existing_element_in_array, "42");
+	TEST(test_find_first_element_in_array, "40");
+	TEST(test_find_last_element_in_array, "44");
+	TEST(test_find_nonexisting_element_in_array, "element not found\n");
+	TEST(test_find_in_empty_array, "element not found\n");
+	TEST(test_find_in_huge_array, "0 1999999");
 
 	return (0);
 }
