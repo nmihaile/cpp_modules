f664afdd77d43c6151daee89abbfe663a4dfa029
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-18 11:52:37 +0100

    ex00: fixes easyfind constness of arguments and adds an easyfind which respects constcorrectness

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 16:24:54 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/18 10:35:34 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/18 11:47:51 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,18 @@
 #include <stdexcept>
 
 template <typename T>
-typename  T::iterator	easyfind(const T& type, const int& i)
+typename  T::iterator	easyfind(T& type, const int& i)
 {
 	typename T::iterator it = std::find(type.begin(), type.end(), i);
+	if (it == type.end())
+		throw ( std::runtime_error("element not found") );
+	return (it);
+};
+
+template <typename T>
+typename  T::const_iterator	easyfind(const T& type, const int& i)
+{
+	typename T::const_iterator it = std::find(type.begin(), type.end(), i);
 	if (it == type.end())
 		throw ( std::runtime_error("element not found") );
 	return (it);
