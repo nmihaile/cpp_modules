38cdb9a5db35ca4726ccdfda94605c5c476b3357
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-18 16:41:30 +0100

    ex01: adds first easy test from subject to test a little bit

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,15 +6,24 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 15:22:02 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/18 15:34:48 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/18 16:39:37 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Span.hpp"
 
 int	main(void)
 {
-	Span	span(5);
+	Span sp(5);// = Span(5);
+
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	// std::cout << sp.longestSpan() << std::endl;
 
 	return (0);
 }
