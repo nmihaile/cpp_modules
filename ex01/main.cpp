681b3505cbaf3ffcc753a25a5a9723402832ec0f
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-20 10:38:27 +0100

    ex01: moves subject test in test_func and executes test_func in main()

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,14 +6,15 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 15:22:02 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/18 16:47:50 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/20 10:36:15 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Span.hpp"
+#include "tester.hpp"
 
-int	main(void)
+void	test_exmple_from_subject(void)
 {
 	Span sp = Span(5);
 
@@ -24,6 +25,14 @@ int	main(void)
 	sp.addNumber(11);
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
+}
 
+int	main(void)
+{
+
+	TEST(test_exmple_from_subject, "2\n14\n");
+	
+	print_test_result();
+	
 	return (0);
 }
