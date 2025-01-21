ddb28dcdd290da5905f873056a37dc84a89ef2c1
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-21 19:13:12 +0100

    ex02: adds test_basic_MutantStack && printSpace() func in main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,14 +6,21 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 19:55:13 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/21 18:52:05 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/21 19:12:29 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <list>
+#include <iterator>
 #include "MutantStack.hpp"
 #include "tester.hpp"
+
+template <typename IT>
+std::string printSpace(const IT& it, const IT& end)
+{
+	return ((it != end - 1) ? " " : "");
+};
 
 void	test_subject_example_MutantStack_vs_list(void)
 {
@@ -63,10 +70,27 @@ void	test_subject_example_list(void)
 	}
 }
 
+void	test_basic_MutantStack(void)
+{
+	MutantStack<int> ms;
+	ms.push(1);
+	ms.push(2);
+	ms.push(3);
+	ms.push(4);
+	ms.push(5);
+	ms.push(6);
+	ms.push(42);
+
+	for (MutantStack<int>::iterator it = ms.begin(); it < ms.end(); ++it)
+		std::cout << *it << printSpace<MutantStack<int>::iterator>(it, ms.end());
+
+}
+
 int	main(void)
 {
 
 	TEST(test_subject_example_MutantStack_vs_list, captureStdout(test_subject_example_list));
+	TEST(test_basic_MutantStack, "1 2 3 4 5 6 42");
 
 	print_test_result();
 
