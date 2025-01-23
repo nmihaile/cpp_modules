d518c1a18553f615abbcabff2de534ff3e5794c8
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-23 12:33:30 +0100

    ex00: adds inital BitcoinExchange class

---
+++ ex00/BitcoinExchange.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 09:55:01 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/23 12:21:16 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other)
+{
+	if (this != &other)
+	{
+	}
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
+
+
+BitcoinExchange&	BitcoinExchange::operator=(const BitcoinExchange& rhs)
+{
+	if (this != &rhs)
+	{
+		// TODO: handle dailyBtcPrices 
+	}
+}
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+bool	BitcoinExchange::loadPricetable(void)
+{
+	std::fstream	fs;
+
+	fs.open("data.csv");
+	if (fs.is_open() == false)
+		throw ( std::runtime_error("Failed to open price data, provide a valid data.csv file."));
+
+	
+
+	fs.close();
+}
+
---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 09:55:03 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/23 12:32:51 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <map>
+#include <chrono>
+#include <fstream>
+#include <stdexcept>
+
+class BitcoinExchange
+{
+public:
+	typedef std::chrono::system_clock::time_point t_time_point;
+
+	BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange& other);
+	~BitcoinExchange();
+
+	BitcoinExchange&	operator=(const BitcoinExchange& rhs);
+
+	bool	loadPricetable(void);
+
+private:
+	std::map<t_time_point, uint64_t>	price_table;
+};
+
+#endif
