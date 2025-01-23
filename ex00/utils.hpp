261c7d759adc238949d07656ea3114f7e4afd9b2
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-23 12:31:44 +0100

    ex00: adds utils.hpp .cpp with exit_with_msg()

---
+++ ex00/utils.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 12:00:27 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/23 12:11:53 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <iostream>
+#include <string>
+
+void	exit_with_msg(std::string msg);
+
+#endif
