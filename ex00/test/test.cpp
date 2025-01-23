18f2a47e06732391fd4917a02607e318a5b0fee5
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-23 17:09:49 +0100

    ex00: adds first test: test_instantiating_btcEx

--- ex00/test/test.cpp
+++ ex00/test/test.cpp
@@ -6,18 +6,23 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 16:50:17 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/23 17:03:35 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/23 17:08:17 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tester.hpp"
 #include "../BitcoinExchange.hpp"
 
-int	main(void)
+void	test_instantiating_btcEx(void)
 {
 	BitcoinExchange btcEx;
 	btcEx.loadPriceTable();
+}
 
+int	main(void)
+{
+
+	TEST(test_instantiating_btcEx, "");
 
 	print_test_result();
 
