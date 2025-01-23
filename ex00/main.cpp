18243afc005ed988f20958a32d068f94ef4ef851
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-23 12:34:05 +0100

    ex00: uses utils in main and checks for ac

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,17 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 09:49:15 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/23 09:50:31 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/23 12:11:58 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "utils.hpp"
+
 int	main(int ac, char **av)
 {
-	(void)ac;
+	if (ac != 2)
+		exit_with_msg("usage: ./btc <input.txt>");
+
 	(void)av;
 
 	return (0);
