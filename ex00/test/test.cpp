7e43a6edc99b89a70703544bbdf982b5dd4b192b
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-25 13:02:30 +0100

    ex00: updates loadPriceTable() func call with parameter && cleans up tests of now unnecessary system calls

--- ex00/test/test.cpp
+++ ex00/test/test.cpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 16:50:17 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/25 12:36:18 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/25 12:59:56 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,17 @@
 void	test_instantiating_btcEx(void)
 {
 	BitcoinExchange btcEx;
-	btcEx.loadPriceTable();
+	btcEx.loadPriceTable("data.csv");
 }
 
 void	test_open_data_csv_missing_file(void)
 {
-	system("mv data.csv bdata.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("missing_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("mv bdata.csv data.csv");
 }
 
 void	test_open_data_csv_without_permissions(void)
@@ -37,7 +35,7 @@ void	test_open_data_csv_without_permissions(void)
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
 	system("chmod 644 data.csv");
@@ -45,179 +43,155 @@ void	test_open_data_csv_without_permissions(void)
 
 void	test_inavlid_header_of_data_csv(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"2009-01-02,0\n2009-01-05,0\n\" > data.csv");
+	system("echo \"2009-01-02,0\n2009-01-05,0\n\" > header_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("header_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f header_data.csv");
 }
 
 void	test_missing_delimiter(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n2009-01-021000\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n2009-01-021000\n2009-01-05,0\n\" > delimiter_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("delimiter_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f delimiter_data.csv");
 }
 
 void	test_empty_date(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n,1000\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n,1000\n2009-01-05,0\n\" > empty_date_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("empty_date_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f empty_date_data.csv");
 }
 
 void	test_empty_price(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n2009-01-04,\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n2009-01-04,\n2009-01-05,0\n\" > empty_price_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("empty_price_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f empty_price_data.csv");
 }
 
 void	test_invalid_date_format_01_ordering(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n2009-30-01,500\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n2009-30-01,500\n2009-01-05,0\n\" > format01_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("format01_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f format01_data.csv");
 }
 
 void	test_invalid_date_format_02_short_year(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n09-01-01,500\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n09-01-01,500\n2009-01-05,0\n\" > format02_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("format02_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f format02_data.csv");
 }
 
 void	test_invalid_date_format_03_non_digit(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n2009-0a-01,500\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n2009-0a-01,500\n2009-01-05,0\n\" > format03_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("format03_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f format03_data.csv");
 }
 
 void	test_invalid_date_format_04_no_dashes(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n2009-01_01,500\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n2009-01_01,500\n2009-01-05,0\n\" > format04_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("format04_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f format04_data.csv");
 }
 
 void	test_invalid_date_day_to_high(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n2009-01-32,500\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n2009-01-32,500\n2009-01-05,0\n\" > day_high_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("day_high_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f day_high_data.csv");
 }
 
 void	test_invalid_date_month_to_high(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n2009-13-01,500\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n2009-13-01,500\n2009-01-05,0\n\" > month_high_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("month_high_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f month_high_data.csv");
 }
 
 void	test_invalid_date_day_to_low(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n2009-12-00,500\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n2009-12-00,500\n2009-01-05,0\n\" > day_low_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("day_low_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f day_low_data.csv");
 }
 
 void	test_invalid_date_month_to_low(void)
 {
-	system("mv data.csv bdata.csv");
-	system("echo \"date,exchange_rate\n2009-00-01,500\n2009-01-05,0\n\" > data.csv");
+	system("echo \"date,exchange_rate\n2009-00-01,500\n2009-01-05,0\n\" > month_low_data.csv");
 	try
 	{
 		BitcoinExchange btcEx;
-		btcEx.loadPriceTable();
+		btcEx.loadPriceTable("month_low_data.csv");
 	}
 	catch(const std::exception& e) { std::cout << e.what() << '\n'; }	
-	system("rm -f data.csv");
-	system("mv bdata.csv data.csv");
+	system("rm -f month_low_data.csv");
 }
 
 int	main(void)
 {
 
 	TEST(test_instantiating_btcEx, "");
-	TEST(test_open_data_csv_missing_file, "Failed to open data.csv, provide a valid data.csv file with read permissions.\n");
+	TEST(test_open_data_csv_missing_file, "Failed to open missing_data.csv, provide a valid missing_data.csv file with read permissions.\n");
 	TEST(test_open_data_csv_without_permissions, "Failed to open data.csv, provide a valid data.csv file with read permissions.\n");
-	TEST(test_inavlid_header_of_data_csv, "Invalid header in data.csv, expected: date,exchange_rate\n");
+	TEST(test_inavlid_header_of_data_csv, "Invalid header in header_data.csv, expected: date,exchange_rate\n");
 	TEST(test_missing_delimiter, "Invalid input: delimiter missing: <2009-01-021000>\n");
 	TEST(test_empty_date, "Invalid input: empty date: <,1000>\n");
 	TEST(test_empty_price, "Invalid input: empty price: <2009-01-04,>\n");
