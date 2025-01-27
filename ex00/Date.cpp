57474da3f620a04e37ba04cfb093d4cd6c1ef0c1
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-27 17:16:43 +0100

    ex00: implements CopyConstructor and CopyAssignmentOperator overlaod of Date class

--- ex00/Date.cpp
+++ ex00/Date.cpp
@@ -6,17 +6,17 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 11:22:02 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/01/27 15:06:33 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/01/27 17:12:37 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Date.hpp"
 
-Date::Date() : m_date((std::tm){0, 0, 0, 0, 0, 70, 0, 1})
+Date::Date() : m_date(std::tm{})
 {
 }
 
-Date::Date(std::string& _date) : m_date((std::tm){0})
+Date::Date(std::string& _date) : m_date(std::tm{})
 {
 	parseDateStr(_date);
 }
@@ -24,7 +24,7 @@ Date::Date(std::string& _date) : m_date((std::tm){0})
 Date::Date(const Date& other)
 {
 	if (this != &other)
-	{}
+		this->m_date = other.m_date;
 }
 
 Date::~Date()
@@ -33,7 +33,7 @@ Date::~Date()
 Date&	Date::operator=(const Date& rhs)
 {
 	if (this != &rhs)
-	{}
+		this->m_date = rhs.m_date;
 	return (*this);
 }
 
@@ -54,32 +54,36 @@ std::string	Date::toString(void) const
 /* ************************************************************************** */
 /* ************************************************************************** */
 
+
 void	Date::parseDateStr(std::string& _date)
 {
 	std::stringstream	ss(_date);
 	char				delimiter;
 
+	if (_date.length() != 10)
+		throw ( std::runtime_error("wrong length of date (expecet format YYYY-MM-DD)") );
+
 	ss >> m_date.tm_year;
 	if (ss.fail())
-		throw ( std::runtime_error("invalid year") );
+		throw ( std::runtime_error("failed to parse year") );
 	m_date.tm_year -= 1900;
 
 	ss >> delimiter;
 	if (ss.fail())
-		throw ( std::invalid_argument("invalid date format") );
+		throw ( std::invalid_argument("failed to parsing delimiter") );
 	else if (delimiter != '-')
-		throw ( std::invalid_argument("invalid delimiter in date") );
+		throw ( std::invalid_argument("wrong delimiter in date") );
 
 	ss >> m_date.tm_mon;
 	if (ss.fail())
-		throw ( std::invalid_argument("invalid month") );
+		throw ( std::invalid_argument("failed parsing month") );
 	--m_date.tm_mon;
 
 	ss >> delimiter;
 	if (ss.fail())
-		throw ( std::invalid_argument("invalid date format") );
+		throw ( std::invalid_argument("failed to parsing delimiter") );
 	else if (delimiter != '-')
-		throw ( std::invalid_argument("invalid delimiter in date") );
+		throw ( std::invalid_argument("wrong delimiter in date") );
 
 	ss >> m_date.tm_mday;
 	if (ss.fail())
@@ -94,18 +98,19 @@ bool	Date::validateDate() const
 	int days_in_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 	if (m_date.tm_mon < 0 || m_date.tm_mon > 11)
-		throw ( std::invalid_argument("invalid month, out of range 01-12") );
+		throw ( std::invalid_argument("month out of range 01-12") );
 
 	if (isLeapYear(m_date.tm_year))
 		days_in_month[1] = 29;
 
 	if (m_date.tm_mday < 1 || m_date.tm_mday > days_in_month[m_date.tm_mon])
-		throw ( std::invalid_argument("invalid day, out of range 01-" + std::to_string(days_in_month[m_date.tm_mon])) );
+		throw ( std::invalid_argument("day out of range 01-" + std::to_string(days_in_month[m_date.tm_mon])) );
 
+	return (true);
 }
 
 bool	Date::isLeapYear(int year) const
 {
 	year += 1900;
-	return (( year % 4 == 0 && (year % 100 != 0 && year & 400 == 0) ));
+	return (( year % 4 == 0 && (year % 100 != 0 && year % 400 == 0) ));
 }
