1fb2917c6a245b5dad5dcc79e09b620a861f5ec4
nmihaile <nmihaile@student.42heilbronn.de>
2025-01-27 17:58:15 +0100

    ex00: add MonetaryValue class for storing prices and amounts in uint64_t

---
+++ ex00/MonetaryValue.cpp
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MonetaryValue.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 17:22:57 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/27 17:57:21 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MonetaryValue.hpp"
+
+MonetaryValue::MonetaryValue() : m_value(0)
+{
+}
+
+MonetaryValue::MonetaryValue(uint64_t& _value) : m_value(_value)
+{
+}
+
+MonetaryValue::MonetaryValue(std::string& _value) : m_value(0)
+{
+	parseMonetaryValueStr(_value);
+}
+
+MonetaryValue::MonetaryValue(const MonetaryValue& other) : m_value(other.m_value)
+{
+}
+
+MonetaryValue::~MonetaryValue()
+{
+}
+
+
+MonetaryValue&	MonetaryValue::operator=(const MonetaryValue& rhs)
+{
+	if (this != &rhs)
+		m_value = rhs.m_value;
+	return (*this);
+}
+
+uint64_t	MonetaryValue::getValueCents(void) const
+{
+	return (m_value);
+}
+
+double		MonetaryValue::getValueDouble(void) const
+{
+	return (m_value / 100.0);
+}
+
+std::string	MonetaryValue::getValueStr(void) const
+{
+	std::ostringstream	os;
+	os << m_value / 100 << std::setw(2) << std::setfill('0') << m_value % 100;
+	return (os.str());
+}
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+void	MonetaryValue::parseMonetaryValueStr(std::string& _value)
+{
+	int	dec_count = 0;
+	for (std::string::iterator it = _value.begin(); it < _value.end(); ++it)
+	{
+		if (!std::isdigit(*it) && !(*it == '.'))
+			throw ( std::runtime_error("non monetary character detected, allowd [0..9|.]") );
+		if (*it == '.')
+			++dec_count;
+		if (dec_count > 1)
+			throw ( std::runtime_error("found multiple decimal points, only one allowed") );
+	}
+
+	std::stringstream	ss(_value);
+	double				parsedValue;
+
+	ss >> parsedValue;
+	if (ss.fail())
+		throw ( std::runtime_error("failed to convert str to monetary value") );
+
+	// lightweight safeguard for negative values
+	if (parsedValue < 0)
+		throw ( std::runtime_error("negative monetary value not allowd") );
+		
+	m_value = static_cast<uint64_t>(parsedValue * 100.0);
+}
---
+++ ex00/MonetaryValue.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MonetaryValue.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 17:22:59 by nmihaile          #+#    #+#             */
+/*   Updated: 2025/01/27 17:56:50 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef MONETARYVALUE_HPP
+#define MONETARYVALUE_HPP
+
+#include <cstdint>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+class MonetaryValue
+{
+public:
+	MonetaryValue();
+	MonetaryValue(uint64_t& _value);
+	MonetaryValue(std::string& _value);
+	MonetaryValue(const MonetaryValue& other);
+	~MonetaryValue();
+
+	MonetaryValue&	operator=(const MonetaryValue& rhs);
+
+	uint64_t	getValueCents(void) const;
+	double		getValueDouble(void) const;
+	std::string	getValueStr(void) const;
+	
+private:
+	uint64_t	m_value;
+
+	void	parseMonetaryValueStr(std::string& _value);
+};
+
+#endif
