2911b357e0f704201e83f8d7d8817bcfbd2ad2da
nmihaile <nmihaile@student.42heilbronn.de>
2025-02-02 12:11:43 +0100

    ex02: sets up main program in main, prints usage msg and adds try/catch block with initial PmergeMe object

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,15 +6,36 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 16:07:32 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/02/02 11:57:10 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/02/02 12:09:36 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <exception>
+#include <string>
 #include "PmergeMe.hpp"
 
-int	main(void)
+void	printf_exit(std::string msg, int exit_code)
 {
-	PmergeMe	pmergeme;
+	std::cout << msg << std::endl;
+	exit(exit_code);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac <= 1)
+		printf_exit("\033[92musage: \033[95m./PmergeMe <num1> <num2> ... <numN>\033[0m", 1);
+
+	try
+	{
+		PmergeMe	pmergeme;
+		pmergeme.parseArguments(ac, av);
+		pmergeme.sort();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
 	return (0);
 }
