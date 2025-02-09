aec9c194d7db1a8b65648d060a09fd527d1bdf75
nmihaile <nmihaile@student.42heilbronn.de>
2025-02-09 15:48:32 +0100

    ex02: removes insertOrder() test calls in main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 16:07:32 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/02/08 16:33:11 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/02/09 15:47:51 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,6 @@ void	printf_exit(std::string msg, int exit_code)
 	exit(exit_code);
 }
 
-// void	print_seq(std::vector<Item> seq, unsigned int count)
-// {
-// 	std::cout	<< "\033[93m[" << std::setw(2) << count << "] \033[94m";
-// 	for (auto& s : seq)
-// 		std::cout << s.value << " ";
-// 	std::cout << "\033[0m\n";
-// }
-
 int	main(int ac, char **av)
 {
 	if (ac <= 1)
@@ -38,20 +30,6 @@ int	main(int ac, char **av)
 	{
 		VectorPmergeMe	vec_pmergeme;
 		vec_pmergeme.parseArguments(ac, av);
-
-		// print_seq(vec_pmergeme.insertOrder(1), 1);
-		// print_seq(vec_pmergeme.insertOrder(2), 2);
-		// print_seq(vec_pmergeme.insertOrder(3), 3);
-		// print_seq(vec_pmergeme.insertOrder(4), 4);
-		// print_seq(vec_pmergeme.insertOrder(5), 5);
-		// print_seq(vec_pmergeme.insertOrder(6), 6);
-		// print_seq(vec_pmergeme.insertOrder(7), 7);
-		// print_seq(vec_pmergeme.insertOrder(8), 8);
-		// print_seq(vec_pmergeme.insertOrder(9), 9);
-		// print_seq(vec_pmergeme.insertOrder(10), 10);
-		// print_seq(vec_pmergeme.insertOrder(11), 11);
-		// print_seq(vec_pmergeme.insertOrder(12), 12);
-		// print_seq(vec_pmergeme.insertOrder(13), 13);
 
 		vec_pmergeme.print("Before", true, false);
 		vec_pmergeme.sort();
